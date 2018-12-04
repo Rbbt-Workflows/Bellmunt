c27e67f1ec3be3264f8a21cd6c4cf8b698b2eb30
Miguel Vazquez <miguel.vazquez.g@bsc.es>
2018-12-04 10:48:50 +0100

    Support multiplex

--- workflow.rb
+++ workflow.rb
@@ -12,7 +12,7 @@ Workflow.require_workflow "Sequence"
 module Bellmunt
   extend Workflow
   def self.reference
-		'b37'
+    'b37'
   end
 
   def self.path
@@ -20,7 +20,7 @@ module Bellmunt
   end
 
   def self.interval_list
-		Rbbt.share.projects.Bellmunt.data['Padded.hs37d5.bed'].find
+		Rbbt.share.projects.Bellmunt.data['Padded.b37.bed'].find
   end
 
   def self.pon
@@ -32,19 +32,73 @@ module Bellmunt
   end
 
   def self.germline_resource
-    GATK.known_sites.b37["af-only-gnomad.vcf"].produce.find
+    GATK.known_sites.b37["af-only-gnomad.vcf.gz"].produce.find
 	end
 
   dep HTS, :BAM_rescore, :fastq1 => :placeholder, :fastq2 => :placeholder, :reference => :placeholder,
+    :sample_name => :placeholder,
+    :platform_unit => :placeholder,
+    :read_group_name => :placeholder,
+    :sequencing_center => "CNAG",
+    :platform => 'Illuimna',
+    :library_name => 'LN',
     :interval_list => Bellmunt.interval_list do |jobname,options|
 
-    sample = jobname
-    options[:fastq1] = self.path[sample + "_1.fastq.gz"].find
-    options[:fastq2] = self.path[sample + "_2.fastq.gz"].find
+    sample_name = jobname
+
+    options[:sample_name] = sample_name.gsub('_', '.')
 
     options[:reference] = Bellmunt.reference
+    sample_fastqs = []
+    Bellmunt.path.glob("*.fastq.gz").each do |file|
+      basename = File.basename(file)
+      sample_fastqs << file if basename.split("_")[2] == sample_name
+    end
 
-    {:inputs => options, :jobname => jobname}
+    case sample_fastqs.length 
+    when 2
+      machine, lane, sample = File.basename(sample_fastqs.first).split("_")
+      options[:read_group_name] = [machine, lane] * "."
+      options[:platform_unit] = [machine, lane, sample] * "."
+      options[:fastq1] = sample_fastqs.sort.first
+      options[:fastq2] = sample_fastqs.sort.last
+      {:inputs => options, :jobname => jobname}
+    when 4
+      sample_runs = {}
+      sample_fastqs.each do |file|
+        machine, lane, sample = File.basename(file).split("_")
+        sample_runs[[machine,lane]*"."] ||= []
+        sample_runs[[machine,lane]*"."] << file
+      end
+      jobs = []
+      num = 1
+      sample_runs.each do |run_code,files|
+        run_options = {}
+        run_options[:read_group_name] = run_code + "." + sample_name
+        run_options[:platform_unit] = run_code
+        run_options[:fastq1] = files.sort.first
+        run_options[:fastq2] = files.sort.last
+        jobs << {:task => :BAM, :inputs => options.merge(run_options), :jobname => jobname + "_multiplex_" + num.to_s}
+        num += 1
+      end
+      jobs
+    else
+      raise "Number of fastq is not 2 or 4: #{Misc.fingerprint sample_fastqs}"
+    end
+  end
+  dep HTS, :BAM_multiplex, :reference => Bellmunt.reference, :bam_files => :placeholder do |jobname,options, dependencies|
+    if dependencies.flatten.length > 1
+      {:inputs => options.merge(:bam_files => dependencies.flatten.collect{|dep| dep.path})}
+    else
+      []
+    end
+  end
+  dep HTS, :BAM_rescore do |jobname,options, dependencies|
+    if (mutiplex = dependencies.flatten.select{|dep| dep.task_name == :BAM_multiplex}.first)
+      {:inputs => options.merge("HTS#BAM_duplicates" =>  mutiplex)}
+    else
+      []
+    end
   end
   task :BAM => :binary do 
     Open.rm self.path
